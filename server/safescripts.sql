8c73b346d4b8ffca934594a96493c8101594ae6e
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-10-27 16:30:41 -0500

    added new sql tables

--- server/safescripts.sql
+++ server/safescripts.sql
@@ -7,3 +7,35 @@ CREATE TABLE prescription (
 	active BOOLEAN,
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+--Stretch Goal Tables
+
+CREATE TABLE "monday" (
+	"monday_id" serial NOT NULL,
+	"schedule_id" integer NOT NULL,
+	"med_name" varchar(30) NOT NULL,
+	"dosage_unit" varchar(10) NOT NULL,
+	"notes" varchar(2000) NOT NULL,
+	"expiration_date" DATE NOT NULL,
+	"refill_date" DATE NOT NULL,
+	CONSTRAINT monday_pk PRIMARY KEY ("monday_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE "schedule" (
+	"schedule_id" serial NOT NULL,
+	"user_name" varchar(30) NOT NULL,
+	"date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT schedule_pk PRIMARY KEY ("schedule_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "monday" ADD CONSTRAINT "monday_fk0" FOREIGN KEY ("schedule_id") REFERENCES "schedule"("schedule_id");
+
+SELECT * FROM "monday" 
