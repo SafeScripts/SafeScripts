dad4dab23b44e75b450f31220fe2045c894a4068
Chad Brady <chadebrady@Chads-MacBook-Pro-2.local>
2018-10-27 12:01:37 -0500

    Git Ignore, base changes

--- SmartScripts/iOS/SafeScripts/SafeScripts/ViewController.swift
+++ SmartScripts/iOS/SafeScripts/SafeScripts/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    let tableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.backgroundColor = .blue
+        view.addSubview(tableView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sizeThatFits(size: view.bounds.size)
+    }
+    
+    public func sizeThatFits(size: CGSize) {
+        var tableViewFrame = CGRect.zero
+        
+        tableViewFrame.origin.y = view.safeAreaInsets.top
+        tableViewFrame.size.width = size.width
+        tableViewFrame.size.height = size.height / 2
+        
+        tableView.frame = tableViewFrame
+    }
 }
 
