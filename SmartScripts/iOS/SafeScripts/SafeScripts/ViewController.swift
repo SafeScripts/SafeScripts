7c995433d711831d85d5294f0b523ce1f1d7d026
Chad Brady <chadebrady@Chads-MacBook-Pro-2.local>
2018-10-27 11:18:37 -0500

    iOS Added

---
+++ SmartScripts/iOS/SafeScripts/SafeScripts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeScripts
+//
+//  Created by Chad Brady on 10/27/18.
+//  Copyright © 2018 SafeScripts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
